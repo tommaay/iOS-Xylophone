527c124e24875b362b8418cac45ce0dac2a36ac9
ToMMaaY <thuynh629@gmail.com>
2019-05-13 15:54:25 -0700

    got the app to play note1 sound

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController{
     
@@ -19,7 +20,12 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        
+        if let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav") {
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            // Play
+            AudioServicesPlaySystemSound(mySound);
+        }
         
     }
     
